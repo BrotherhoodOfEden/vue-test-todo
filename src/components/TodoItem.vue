97cd2e12a2630a113af9357ccd093869c3aab735
igavelyuk <gavelyukworking@gmail.com>
2019-10-30 11:51:54 +0200

    first half proj done

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-bind:class="{'is-completed':todo.completed}">
+      <p>
+        <input type="checkbox" v-on:change="markComplete">
+        {{todo.title}}
+      <button @click="$emit('del-todo',todo.id)">X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.is-completed{
+  text-decoration: line-through;
+}
+.del{
+  background: red;
+  color: wheat;
+  float: right;
+  border-radius: 50%;
+}
+</style>
